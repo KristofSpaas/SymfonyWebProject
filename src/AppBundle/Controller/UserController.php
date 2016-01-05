dca56054ada44184f0eed7fb71df096d066be183
lg188 <lg188@tfwno.gf>
2016-01-05 12:13:34 +0100

    nerf default registratie naar patient

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -52,7 +52,7 @@ class UserController extends Controller
                 ->setUserPassword($user, $user->getPassword());
 
             //add role based on value off checkbox
-            $user->setRoles(array('ROLE_DOCTOR'));
+            $user->setRoles(array('ROLE_PATIENT'));
             $em->persist($user);
             $em->flush();
 
