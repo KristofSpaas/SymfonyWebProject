4a0564235b36ac6512e0cd51d24e2ba53ca03905
lg188 <lg188@tfwno.gf>
2016-01-26 11:43:19 +0100

    verbetering afspraak controller

--- src/AppBundle/Controller/AfspraakController.php
+++ src/AppBundle/Controller/AfspraakController.php
@@ -23,38 +23,62 @@ class AfspraakController extends Controller
     }
 
     /**
-     * @Route("/addAfspraak")
+     * @Route("/addAfspraak/{id}", name="addAfspraak")
      * @method({"GET", "POST"}
      */
-    public function addAfspraakAction( Request $request )
+    public function addAfspraakAction( Request $request, $id )
     {
-    	$em = $this->getDoctrine()->getManager();
-	$doctors = $em->getRepository("AppBundle:Doctor")->findAll();
-	$patients = $em->getRepository("AppBundle:Patient")->findAll();
+        $em = $this->getDoctrine()->getManager();
+        $doctor = $em->getRepository("AppBundle:Doctor")->find($id);
+        //dump($doctors);
+        $user= $this->get('security.token_storage')->getToken()->getUser();
+        $patients = $em->getRepository("AppBundle:Patient")->findAll();
+        dump($patients);
+        $patientOut=1;
+        foreach($patients as $patient){
+            if($patient->getUser() == $user){
+                $patientOut = $patient;
+                break;
+            }
+        }
+        dump($patient);
 
-	$afspraak = new Afspraak();
-	$form = $this->createForm(AfspraakType::class, $afspraak);
-	$form->handleRequest($request);
+        if($user != "anon."){
+            $roles = $user->getRoles();
+            dump($user);
+            dump($id);
+        }else{
+            echo("ERROR ");
+        }
+        $afspraak = new Afspraak();
+        $form = $this->createForm(AfspraakType::class, $afspraak);
+        $form->handleRequest($request);
 
-	$user= $this->get('security.token_storage')->getToken()->getUser();
+        // dump($roles);
+        if($form->isSubmitted() && $form->isValid()){
+            $afspraak->setDoctor($doctor);
+            $afspraak->setPatient($patient);
+            $afspraak->setBezet("true");
 
-	if($form->isSubmitted() && $form->isValid()){
-		$message = \Swift_Message::newInstance()
-		->setSubject("Appointment on dendokteur")
-		->setFrom("dendokteur@gmail.com")
-		->setTo($user->getEmail())
-		->setContentType('text/html')
-		->setBody( $this->renderView( 'AppBundle:emails:afspraak.html.twig', array( 'afspraak' => $afspraak)), 'text/html');
-		// TODO: Uncomment this to enable emails
-		// $this->get('mailer')->send($message);
+            $em->persist($afspraak);
+            $em->flush();
 
-		return $this->redirectToRoute('showAfspraken');
-	}
+            $message = \Swift_Message::newInstance()
+                     ->setSubject("Appointment on dendokteur")
+                     ->setFrom("dendokteur@gmail.com")
+                     ->setTo($user->getEmail())
+                     ->setContentType('text/html')
+                     ->setBody( $this->renderView( 'AppBundle:emails:afspraak.html.twig', array( 'afspraak' => $afspraak)), 'text/html');
+            // TODO: Uncomment this to enable emails
+            // $this->get('mailer')->send($message);
+
+            return $this->redirectToRoute('showAfspraken');
+        }
 
         return $this->render('AppBundle:Afspraak:addAfspraak.html.twig', array(
-	'afspraak' => $afspraak,
-	'form' => $form->createView(),
-	));
+            'afspraak' => $afspraak,
+            'form' => $form->createView(),
+        ));
     }
 
 }
